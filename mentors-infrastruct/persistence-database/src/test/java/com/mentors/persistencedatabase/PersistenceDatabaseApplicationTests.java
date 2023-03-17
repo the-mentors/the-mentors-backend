497e99357b0d7d7a6eb77e1fdbe193499a9e2646
WoojinDeve <jwj060111@gmail.com>
2023-03-17 14:16:01 +0900

    init: 프로젝트 초기설정

---
+++ mentors-infrastruct/persistence-database/src/test/java/com/mentors/persistencedatabase/PersistenceDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentors.persistencedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mentors-interface/src/test/java/com/mentors/MentorsInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorsInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
