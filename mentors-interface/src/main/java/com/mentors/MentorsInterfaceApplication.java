e3303f7d5252223a5388329db924bd0793104ac6
WoojinDeve <jwj060111@gmail.com>
2023-03-21 19:57:04 +0900

    refactor: asciidoctor 플러그인 버전 수정

--- mentors-interface/src/main/java/com/mentors/MentorsInterfaceApplication.java
+++ mentors-interface/src/main/java/com/mentors/MentorsInterfaceApplication.java
@@ -3,7 +3,7 @@ package com.mentors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class MentorsInterfaceApplication {
 
     public static void main(String[] args) {
