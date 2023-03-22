4c35a316ffe93887f9ae1f0e2552b88cb2b2bb08
WoojinDeve <jwj060111@gmail.com>
2023-03-22 17:12:59 +0900

    feat: 카테고리 테이블 구성 및 flyway 적용

---
+++ mentors-infrastruct/persistence-database/src/main/resources/db/migration/V001__Create_Categories.sql
@@ -0,0 +1,12 @@
+create table if not exists categories
+(
+    category_id bigint PRIMARY KEY AUTO_INCREMENT,
+    category_code bigint unique not null,
+    category_name varchar(50) unique not null,
+    parent_code bigint not null,
+    created_at datetime not null,
+    updated_at datetime not null
+) engine = InnoDB;
+
+create index CATEGORIES__index_category_code
+    on categories (category_code);
