468f4d76b62d16d6d11c8a2caaaf5cdfa833e975
WoojinDeve <jwj060111@gmail.com>
2023-03-22 18:06:00 +0900

    feat: 카테고리 도메인 변환 Mapper 클래스

---
+++ mentors-domain/src/main/java/com/mentors/category/mapper/CategoryDomainMapper.java
@@ -0,0 +1,16 @@
+package com.mentors.category.mapper;
+
+import com.mentors.category.CategoryEntity;
+import com.mentors.category.domain.Category;
+
+public class CategoryDomainMapper {
+
+    public static Category toDomain(CategoryEntity ce){
+        return Category.of(ce.getId(),
+                ce.getCategoryName(),
+                ce.getCategoryCode(),
+                ce.getParentCode(),
+                ce.getCreatedAt(),
+                ce.getUpdatedAt());
+    }
+}
