a706e7ba9018317c7cfb1761f2099e8abf297082
jangwon3828 <wkd3828@gmail.com>
2023-03-23 18:39:46 +0900

    fix: 도메인 유저 중복코드 삭제

--- mentors-domain/src/main/java/com/mentors/user/domain/User.java
+++ mentors-domain/src/main/java/com/mentors/user/domain/User.java
@@ -6,14 +6,5 @@ public record User(
                    String userName,
                    String nickName,
                    String profileUrl) {
-    public static User of(
-                          final String email,
-                          final String password,
-                          final String userName,
-                          final String nickName,
-                          final String profileUrl) {
-        return new User(email, password, userName, nickName, profileUrl);
-    }
-
 }
 
