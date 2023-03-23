95df7e02fb2e8e4e813e971654e7ae6de8d6122a
jangwon3828 <wkd3828@gmail.com>
2023-03-23 19:39:51 +0900

    fix: 중복코드 삭제

--- mentors-domain/src/main/java/com/mentors/user/mapper/UserDomainMapper.java
+++ mentors-domain/src/main/java/com/mentors/user/mapper/UserDomainMapper.java
@@ -5,15 +5,6 @@ import com.mentors.user.domain.User;
 
 public class UserDomainMapper {
 
-    public static User toDomain(UserEntity ue){
-        return User.of(
-                ue.getEmail(),
-                ue.getPassword(),
-                ue.getUsername(),
-                ue.getNickname(),
-                ue.getProfileUrl());
-    }
-
     public static UserEntity toEntity(User u){
         return UserEntity.builder()
                 .email(u.email())
