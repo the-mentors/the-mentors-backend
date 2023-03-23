a2607510f9bd095dd8aed2e2ba32e01a7675c14b
WoojinDeve <jwj060111@gmail.com>
2023-03-23 17:27:30 +0900

    feat: 카테고리 도메인 매퍼 클래스 테스트

---
+++ mentors-domain/src/test/java/com/mentors/category/mapper/CategoryDomainMapperTest.java
@@ -0,0 +1,33 @@
+package com.mentors.category.mapper;
+
+import static com.mentors.support.fixture.CategoryFixture.*;
+
+import com.mentors.category.CategoryEntity;
+import com.mentors.category.domain.Category;
+import com.mentors.support.BasicClassTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CategoryDomainMapperTest extends BasicClassTest {
+
+    @DisplayName("카테고리 도메인을 엔티티로 변환")
+    @Test
+    void givenCategoryDomain_whenTransformingDomain_thenReturnCategoryEntity() {
+        //given & when
+        CategoryEntity entity = CategoryDomainMapper.toEntity(toDomain());
+
+        //then
+        Assertions.assertInstanceOf(CategoryEntity.class, entity);
+    }
+
+    @DisplayName("카테고리 엔티티를 도메인으로 변환")
+    @Test
+    void givenCategoryEntity_whenTransformingEntity_thenReturnCategoryDomain() {
+        //given & when
+        Category domain = CategoryDomainMapper.toDomain(toEntity());
+
+        //then
+        Assertions.assertInstanceOf(Category.class, domain);
+    }
+}
