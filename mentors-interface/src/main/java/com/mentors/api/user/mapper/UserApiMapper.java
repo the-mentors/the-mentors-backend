c432a6c1b83070a3a6c4a00a35124760657a5e49
WoojinDeve <jwj060111@gmail.com>
2023-03-25 22:50:01 +0900

    feat: 유저 수정 클래스 매퍼 도메인 변경 메서드

--- mentors-interface/src/main/java/com/mentors/api/user/mapper/UserApiMapper.java
+++ mentors-interface/src/main/java/com/mentors/api/user/mapper/UserApiMapper.java
@@ -1,11 +1,23 @@
 package com.mentors.api.user.mapper;
 
+import com.mentors.api.user.dto.UserEditRequest;
 import com.mentors.api.user.dto.UserSignUpRequest;
 import com.mentors.user.domain.User;
 
 public class UserApiMapper {
 
     public static User toDomain(UserSignUpRequest usr, String password) {
-        return new User(null,usr.email(),password, usr.userName(), usr.nickName(), usr.profileUrl(),null,null);
+        return new User(null, usr.email(), password, usr.userName(), usr.nickName(), usr.profileUrl(), null, null);
+    }
+
+    public static User toDomain(final UserEditRequest request) {
+        return new User(null,
+                null,
+                null,
+                request.userName(),
+                request.nickName(),
+                request.profileUrl(),
+                null,
+                null);
     }
 }
