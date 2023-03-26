55bfdba9630aba4258ab62789da972e0e2bc4753
jangwon3828 <wkd3828@gmail.com>
2023-03-26 10:58:18 +0900

    feat: 인증 엔티티 구성

---
+++ mentors-infrastruct/persistence-database/src/main/java/com/mentors/auth/AuthEntity.java
@@ -0,0 +1,30 @@
+package com.mentors.auth;
+
+import com.mentors.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@Table(name = "auths")
+@NoArgsConstructor(access = PROTECTED)
+public class AuthEntity extends BaseEntity {
+    @Id
+    private Long memberId;
+
+    private String refreshToken;
+
+
+    @Builder
+    public AuthEntity(Long memberId, String refreshToken) {
+        this.memberId = memberId;
+        this.refreshToken = refreshToken;
+    }
+
+}
