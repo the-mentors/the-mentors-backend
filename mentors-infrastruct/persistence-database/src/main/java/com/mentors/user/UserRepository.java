7eca383e7efe003d9b9d1edf2b7b8cd10e3dcd26
jangwon3828 <wkd3828@gmail.com>
2023-03-26 14:32:50 +0900

    feat: 이메일로 유저찾기 추가

--- mentors-infrastruct/persistence-database/src/main/java/com/mentors/user/UserRepository.java
+++ mentors-infrastruct/persistence-database/src/main/java/com/mentors/user/UserRepository.java
@@ -2,7 +2,15 @@ package com.mentors.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
 
+    Optional<UserEntity>  findByEmail(String email);
+
+    default UserEntity getByEmail(final String email){
+        return findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 회원입니다."));
+    }
 }
