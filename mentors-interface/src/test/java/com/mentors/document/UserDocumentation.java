34e7d074ae9030a9cdc911aea2ab60b2a16e7bb1
jangwon3828 <wkd3828@gmail.com>
2023-03-26 09:10:50 +0900

    feat: 유저 회원가입 API테스트

--- mentors-interface/src/test/java/com/mentors/document/UserDocumentation.java
+++ mentors-interface/src/test/java/com/mentors/document/UserDocumentation.java
@@ -1,21 +1,49 @@
 package com.mentors.document;
 
-import static org.mockito.Mockito.doNothing;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.mentors.support.fixture.UserFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import static com.mentors.support.fixture.UserFixture.회원가입_요청정보;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class UserDocumentation extends Documentation{
 
+    @DisplayName("[Docs]유저 회원가입")
+    @Test
+    void givenSignUpUserRequest_whenSignUpUser_thenReturnIsCreated() throws Exception {
+        willDoNothing().given(signUpUserUsecase).execute(회원가입_요청정보());
+        // expected
+        mockMvc.perform(post("/api/v1/users/signup")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(회원가입_요청정보())
+                        )
+                )
+                .andExpect(status().isCreated())
+                .andDo(document("user/post/signup",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("email").description("이메일"),
+                                fieldWithPath("password").description("비밀번호"),
+                                fieldWithPath("username").description("이름"),
+                                fieldWithPath("nickname").description("닉네임"),
+                                fieldWithPath("profileUrl").description("프로필 이미지")
+                        )
+                ));
+
+    }
     @DisplayName("[Docs] 유저 정보 수정")
     @Test
     void givenUpdateUserRequest_whenUpdating_thenReturnStatusIsOk() throws Exception {
