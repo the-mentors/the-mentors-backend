2b309d7767567fd15c7cb597ece152c965df4043
jangwon3828 <wkd3828@gmail.com>
2023-03-26 21:02:41 +0900

    refactor: 잘못된 시크릿키 전달 방식 수정 및 토큰 만료 검증 추가

--- mentors-interface/src/main/java/com/mentors/global/jwt/JwtTokenProvider.java
+++ mentors-interface/src/main/java/com/mentors/global/jwt/JwtTokenProvider.java
@@ -1,21 +1,25 @@
 package com.mentors.global.jwt;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+@Component
 public class JwtTokenProvider implements TokenProvider {
 
     private final SecretKey key;
     private final long accessTokenValidityInMilliseconds;
     private final long refreshTokenValidityInMilliseconds;
 
-    public JwtTokenProvider(@Value("${jwt.token.secret}") final SecretKey key,
+    public JwtTokenProvider(@Value("${jwt.token.secret}") final String secretKey,
                             @Value("${jwt.token.access.expiration}") long accessTokenValidityInMilliseconds,
                             @Value("${jwt.token.refresh.expiration}") long refreshTokenValidityInMilliseconds) {
-        this.key = key;
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
         this.accessTokenValidityInMilliseconds = accessTokenValidityInMilliseconds;
         this.refreshTokenValidityInMilliseconds = refreshTokenValidityInMilliseconds;
     }
@@ -23,14 +27,15 @@ public class JwtTokenProvider implements TokenProvider {
 
     @Override
     public String createAccessToken(String payload) {
-        return createToken(payload,accessTokenValidityInMilliseconds);
-    }
-    @Override
-    public String createRefreshToken(String payload) {
-        return createToken(payload,refreshTokenValidityInMilliseconds);
+        return createToken(payload, accessTokenValidityInMilliseconds);
     }
 
-    private String createToken(String payload,Long validityInMilliseconds) {
+    @Override
+    public String createRefreshToken(String payload) {
+        return createToken(payload, refreshTokenValidityInMilliseconds);
+    }
+
+    private String createToken(String payload, Long validityInMilliseconds) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
 
@@ -59,12 +64,16 @@ public class JwtTokenProvider implements TokenProvider {
                     .setSigningKey(key)
                     .build()
                     .parseClaimsJws(token);
-            claims.getBody()
-                    .getExpiration()
-                    .before(new Date());
+            tokenExpireValidate(claims);
             return true;
         } catch (final JwtException | IllegalArgumentException e) {
             throw new RuntimeException("권한이 없습니다.");
+        }
+    }
+
+    private static void tokenExpireValidate(Jws<Claims> claims) {
+        if (claims.getBody().getExpiration().before(new Date())){
+            throw new RuntimeException("만료된 토큰입니다.");
         }
     }
 
