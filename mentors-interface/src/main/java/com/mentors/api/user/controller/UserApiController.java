6f15302c1a6e267daad1153c22af53e5cd23ce3c
jangwon3828 <wkd3828@gmail.com>
2023-03-30 17:12:32 +0900

    refactor: 유저 인터페이스 토큰값 제거 및 인증정보 추가

--- mentors-interface/src/main/java/com/mentors/api/user/controller/UserApiController.java
+++ mentors-interface/src/main/java/com/mentors/api/user/controller/UserApiController.java
@@ -6,11 +6,12 @@ import com.mentors.api.user.dto.UserSignUpRequest;
 import com.mentors.api.user.usecase.EditUserUsecase;
 import com.mentors.api.user.usecase.SignInUserUsecase;
 import com.mentors.api.user.usecase.SignUpUserUsecase;
-import com.mentors.user.authToken.domain.AuthToken;
+import com.mentors.global.auth.dto.UserInfo;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,17 +32,17 @@ public class UserApiController {
 
     @PutMapping
     public ResponseEntity<Void> updateUser(
-            @RequestParam final Long id,
+            @AuthenticationPrincipal final UserInfo userInfo,
             @RequestBody @Valid final UserEditRequest request
     ){
-        editUserUsecase.execute(id, request);
+        editUserUsecase.execute(userInfo.userId(), request);
         return ResponseEntity.ok().build();
     }
     @PostMapping("/signin")
-    public ResponseEntity<AuthToken> signUpUser(
+    public ResponseEntity<Void> signUpUser(
             @RequestBody @Valid UserSignInRequest userSignInRequest
     ) {
-        AuthToken authToken = signInUserUsecase.execute(userSignInRequest);
-        return ResponseEntity.ok(authToken);
+        signInUserUsecase.execute(userSignInRequest);
+        return ResponseEntity.ok().build();
     }
 }
