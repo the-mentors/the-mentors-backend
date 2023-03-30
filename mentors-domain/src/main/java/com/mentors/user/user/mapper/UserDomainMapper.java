15783d16658be60bd25f6ab78fec610dccf94032
jangwon3828 <wkd3828@gmail.com>
2023-03-30 16:56:04 +0900

    feat: 유저 엔티티에서 도메인 변환, 도메인에서 권한을 유저로 엔티티로 변환 추가

--- mentors-domain/src/main/java/com/mentors/user/user/mapper/UserDomainMapper.java
+++ mentors-domain/src/main/java/com/mentors/user/user/mapper/UserDomainMapper.java
@@ -1,11 +1,26 @@
 package com.mentors.user.user.mapper;
 
+import com.mentors.authority.Authority;
+import com.mentors.global.common.Role;
 import com.mentors.user.user.UserEntity;
 import com.mentors.user.user.domain.User;
 
+import java.util.ArrayList;
+
 public class UserDomainMapper {
 
-    public static UserEntity toEntity(User user){
+    public static UserEntity toEntityWithRoleUser(User user, String password) {
+        return UserEntity.builder()
+                .email(user.email())
+                .password(password)
+                .username(user.username())
+                .nickname(user.nickname())
+                .profileUrl(user.profileUrl())
+                .role(Role.USER)
+                .build();
+    }
+
+    public static UserEntity toEntity(User user) {
         return UserEntity.builder()
                 .email(user.email())
                 .password(user.password())
@@ -13,5 +28,19 @@ public class UserDomainMapper {
                 .nickname(user.nickname())
                 .profileUrl(user.profileUrl())
                 .build();
+    }
+
+    public static User toDomain(UserEntity user) {
+        ArrayList<Authority> arrayListRole =new ArrayList<>();
+        arrayListRole.add((Authority) user.getAuthorities());
+        return new User(user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getUsername(),
+                user.getNickname(),
+                user.getProfileUrl(),
+                arrayListRole,
+                user.getCreatedAt(),
+                user.getUpdatedAt());
     }
 }
