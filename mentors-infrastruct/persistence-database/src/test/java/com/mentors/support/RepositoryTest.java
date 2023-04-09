a376e7bf77bc727fb79cb7b79d4aaa82953ed91e
jangwon3828 <wkd3828@gmail.com>
2023-04-09 23:32:44 +0900

    refactor: 상속받는 클래스까지 가져오기위한 설정 추가

--- mentors-infrastruct/persistence-database/src/test/java/com/mentors/support/RepositoryTest.java
+++ mentors-infrastruct/persistence-database/src/test/java/com/mentors/support/RepositoryTest.java
@@ -1,13 +1,22 @@
 package com.mentors.support;
 
+import com.mentors.global.config.JpaConfiguration;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
 
 @Profile("test")
-@DataJpaTest
+@Transactional
+@DataJpaTest(includeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = JpaConfiguration.class
+))
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class RepositoryTest {
+
 
 }
