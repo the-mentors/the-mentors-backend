99c9f21c5d8ff5177e8b428110b92203af3a768b
jangwon3828 <wkd3828@gmail.com>
2023-04-09 21:37:27 +0900

    feat: 데이터베이스 테스트 코드 중복을 없애는 상위 클래스

---
+++ mentors-infrastruct/persistence-database/src/test/java/com/mentors/TestSpringbootContainer.java
@@ -0,0 +1,7 @@
+package com.mentors;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.mentors")
+public class TestSpringbootContainer {
+}
---
+++ mentors-infrastruct/persistence-database/src/test/java/com/mentors/support/RepositoryTest.java
@@ -0,0 +1,13 @@
+package com.mentors.support;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@DataJpaTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class RepositoryTest {
+
+}
