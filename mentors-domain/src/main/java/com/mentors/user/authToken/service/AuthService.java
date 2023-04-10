9eb313da8b43e5a12507e2b4f22ab8ac0083346e
jangwon3828 <wkd3828@gmail.com>
2023-04-10 14:02:00 +0900

    refactor: 매개변수명 통일

--- mentors-domain/src/main/java/com/mentors/user/authToken/service/AuthService.java
+++ mentors-domain/src/main/java/com/mentors/user/authToken/service/AuthService.java
@@ -2,10 +2,10 @@ package com.mentors.user.authToken.service;
 
 
 public interface AuthService {
-    String getAuthToken(Long userId);
+    String getAuthToken(Long key);
 
-    boolean existAuthToken(Long userId);
+    boolean existAuthToken(Long key);
 
-    String saveAuthToken(Long userId,String refreshToken);
+    String saveAuthToken(Long key,String refreshToken);
     void ifExistAuthTokenDelete(Long key);
 }
