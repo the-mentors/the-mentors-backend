e852e37fd206bc050f8e83a0021b5105e344062a
jangwon3828 <wkd3828@gmail.com>
2023-04-10 15:14:03 +0900

    feat: 인증 Fixture 생성

---
+++ mentors-domain/src/testFixtures/java/com/mentors/AuthFixture.java
@@ -0,0 +1,8 @@
+package com.mentors;
+
+public class AuthFixture {
+
+    public static final Long key = 1L;
+    public static final Long unSavedKey = 0L;
+    public static final String refreshToken = "aaaaaaaaaaaa.bbbbbbbbbbbbb.cccccccccccccc";
+}
