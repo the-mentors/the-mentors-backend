9fb2a081bad559ede379f1ecd693c9af067c8ea7
jangwon3828 <wkd3828@gmail.com>
2023-04-11 17:04:59 +0900

    feat: 인증Fixture 값 추가

--- mentors-domain/src/testFixtures/java/com/mentors/AuthFixture.java
+++ mentors-domain/src/testFixtures/java/com/mentors/AuthFixture.java
@@ -1,8 +1,14 @@
 package com.mentors;
 
+import com.mentors.user.authToken.domain.AuthToken;
+
 public class AuthFixture {
 
     public static final Long key = 1L;
     public static final Long unSavedKey = 0L;
+    public static final String accessToken = "bbbbbbbbbbbbb.aaaaaaaaaaaa.cccccccccccccc";
     public static final String refreshToken = "aaaaaaaaaaaa.bbbbbbbbbbbbb.cccccccccccccc";
+    public static AuthToken toAuthToken(){
+        return new AuthToken(accessToken,refreshToken);
+    }
 }
