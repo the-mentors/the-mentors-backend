4d594689cdb2bc18ce13219e2edda9993486bd4d
jangwon3828 <wkd3828@gmail.com>
2023-04-11 17:12:05 +0900

    feat: DocumentationFixture에 Login에 필요한 정보 추가

--- mentors-interface/src/test/java/com/mentors/document/DocumentationFixture.java
+++ mentors-interface/src/test/java/com/mentors/document/DocumentationFixture.java
@@ -1,10 +1,33 @@
 package com.mentors.document;
 
+import com.mentors.AuthFixture;
 import com.mentors.CategoryFixture;
+import com.mentors.UserFixture;
+import com.mentors.authority.Authority;
 import com.mentors.category.domain.Category;
+import com.mentors.user.Role;
+import com.mentors.user.auth.UserContext;
+import com.mentors.user.authToken.domain.AuthToken;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class DocumentationFixture {
 
-    public static List<Category> CATEGORIES_FIXTURE = CategoryFixture.toSortCategories();
+    public static final List<Category> CATEGORIES_FIXTURE = CategoryFixture.toSortCategories();
+    public static final AuthToken AUTHTOKEN_FIXTURE= AuthFixture.toAuthToken();
+    public static final String EMAIL = "user1@email.com";
+    public static final List<String> ROLE = List.of("USER");
+
+
+
+    public static UserContext USERCONTEST_FIXTURE = new UserContext(UserFixture.toDomainWithRoleAndUserId(),changeStringToAuthorityByRole(UserFixture.toDomainWithRole().role()));
+
+    private static ArrayList<Authority> changeStringToAuthorityByRole(ArrayList<String> roles) {
+        ArrayList<Authority> authorities = new ArrayList<>();
+        roles.forEach(role->authorities.add(new Authority(Enum.valueOf(Role.class, role))));
+        return authorities;
+    }
+
+
 }
