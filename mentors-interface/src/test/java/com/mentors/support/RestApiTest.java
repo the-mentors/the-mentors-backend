f95d099970c4ab23ea1ce4a4f07c9b7185af429c
WoojinDeve <jwj060111@gmail.com>
2023-04-11 12:58:13 +0900

    feat: 엑세스 토큰 주식 메서드 추가

--- mentors-interface/src/test/java/com/mentors/support/RestApiTest.java
+++ mentors-interface/src/test/java/com/mentors/support/RestApiTest.java
@@ -1,7 +1,12 @@
 package com.mentors.support;
 
+import static com.mentors.UserFixture.toDomain;
+import static com.mentors.support.fixture.UserFixture.로그인_요청정보;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mentors.lib.DatabaseCleaner;
+import com.mentors.user.user.service.UserWriteService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -10,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
@@ -20,16 +26,18 @@ public class RestApiTest {
 
     @LocalServerPort
     protected int port;
+    protected TestRestTemplate rest;
+
+    @Autowired
+    private UserWriteService userWriteService;
 
     @Autowired
     private DatabaseCleaner databaseCleaner;
-    protected TestRestTemplate rest;
 
     @Autowired
     protected ObjectMapper objectMapper;
 
     protected HttpHeaders headers;
-
 
     @BeforeEach
     void initialize(){
@@ -37,5 +45,25 @@ public class RestApiTest {
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         databaseCleaner.execute();
+    }
+
+
+    protected void initializeSaveUser(){
+        userWriteService.signUp(toDomain());
+    }
+
+    protected void injectAccessToken() throws JsonProcessingException {
+        var url = String.format("http://localhost:%d/api/v1/users/signin", this.port);
+        var request = new HttpEntity<>(로그인_요청정보(), headers);
+        var json = this.rest.postForEntity(url, request, String.class).getBody();
+        BodyAccessTokenParser bodyParser = objectMapper.readValue(json, BodyAccessTokenParser.class);
+        headers.add(HttpHeaders.AUTHORIZATION, bodyParser.accessToken());
+    }
+
+    private record BodyAccessTokenParser(Body body) {
+        public record Body(String accessToken, String refreshToken){ }
+        public String accessToken(){
+            return "Bearer " + body.accessToken;
+        }
     }
 }
