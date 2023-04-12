542e08fe6124089a073b8dd221b7035ffcda38c9
WoojinDeve <jwj060111@gmail.com>
2023-04-13 01:47:18 +0900

    refactor: @Trasactional 제거 및 @Import 사용

--- mentors-infrastruct/persistence-database/src/test/java/com/mentors/support/RepositoryTest.java
+++ mentors-infrastruct/persistence-database/src/test/java/com/mentors/support/RepositoryTest.java
@@ -6,17 +6,13 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @Profile("test")
-@Transactional
-@DataJpaTest(includeFilters = @ComponentScan.Filter(
-        type = FilterType.ASSIGNABLE_TYPE,
-        classes = JpaConfiguration.class
-))
+@DataJpaTest
+@Import(JpaConfiguration.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class RepositoryTest {
-
-
 }
