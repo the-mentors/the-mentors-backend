48db7f4a9ec513ba1d530a46fd771d7fa23ebcd9
WoojinDeve <jwj060111@gmail.com>
2023-04-13 17:14:48 +0900

    fix: ArrayList -> List로 변경

--- mentors-interface/src/test/java/com/mentors/document/DocumentationFixture.java
+++ mentors-interface/src/test/java/com/mentors/document/DocumentationFixture.java
@@ -11,22 +11,23 @@ import com.mentors.user.authToken.domain.AuthToken;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DocumentationFixture {
 
     public static final List<Category> CATEGORIES_FIXTURE = CategoryFixture.toSortCategories();
-    public static final AuthToken AUTHTOKEN_FIXTURE= AuthFixture.toAuthToken();
+    public static final AuthToken AUTHTOKEN_FIXTURE = AuthFixture.toAuthToken();
     public static final String EMAIL = "user1@email.com";
     public static final List<String> ROLE = List.of("USER");
 
 
+    public static UserContext USERCONTEST_FIXTURE = new UserContext(UserFixture.toDomainWithRoleAndUserId(),
+            convertStringToAuthorityByRole(UserFixture.toDomainWithRole().role()));
 
-    public static UserContext USERCONTEST_FIXTURE = new UserContext(UserFixture.toDomainWithRoleAndUserId(),changeStringToAuthorityByRole(UserFixture.toDomainWithRole().role()));
-
-    private static ArrayList<Authority> changeStringToAuthorityByRole(ArrayList<String> roles) {
-        ArrayList<Authority> authorities = new ArrayList<>();
-        roles.forEach(role->authorities.add(new Authority(Enum.valueOf(Role.class, role))));
-        return authorities;
+    private static List<Authority> convertStringToAuthorityByRole(List<String> roles) {
+        return roles.stream()
+                .map(role -> new Authority(Role.findByName(role)))
+                .collect(Collectors.toList());
     }
 
 
